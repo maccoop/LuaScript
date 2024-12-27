65be597c23be347a8a0cb11a5b863cf409609934
maccoop <89531372+maccoop@users.noreply.github.com>
2024-12-27 14:16:54 +0700

    Update helloWorld.lua

--- helloWorld.lua
+++ helloWorld.lua
@@ -2,26 +2,19 @@ a = 0
 b = 0
 next = 0
 delay = 0.5
+obj = null
 
 function start()
 	print("Start...")
 	a = 0
 	b = 0
 	next = 0
-	local obj = CS.UnityEngine.GameObject("Create Obj")
+	obj = CS.UnityEngine.GameObject("Create Obj")
 	local render = obj:AddComponent(typeof(CS.UnityEngine.SpriteRenderer))
 	local loader = obj:AddComponent(typeof(CS.ImageLoader))
 	loader:LoadImage("https://picsum.photos/200/300", render)
-
 end
 
 function update()
-	print("Update...")
-	if(CS.UnityEngine.Time.time > next)
-	then
-		a = a + 1
-		b = b + 2
-		next = CS.UnityEngine.Time.time + delay
-		print(string.format("haa = %s, bbb = %s",a,b))
-	end
+	obj:transform:posiion = CS.UnityEngine.Input.mousePosition
 end
