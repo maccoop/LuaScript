9deeb47eb4b42640399867547775aa0d7e85c8e1
Sang Vu <sang.vuduc@adnx.com.vn>
2025-01-02 15:57:35 +0700

    ok

---
+++ luaHotFixEx.lua
@@ -0,0 +1,14 @@
+local main = CS.Main
+xlua.hotfix(main, "Update", function(self)
+    local first = CS.DateTime.Now
+    for i = 1,10000
+    then
+        print("call at " ... self.Index ... ": " + i);
+    end
+    local second = CS.DateTime.Now
+    local difference = second:Subtract(first) -- Use the Subtract method
+    print("Time difference in milliseconds: " .. difference.TotalMilliseconds)
+end)
+xlua.hotfix(main, "Start", function(self)
+    print("Create from hotfix")
+end)
